0e401a1fc25cc096eb9cd457d9afa6761353baed
Shriyesh <domshriyesh@gmail.com>
2026-01-27 22:39:05 +0530

    db connected

---
+++ server/routes/educatorRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { updateRoleEducator } from '../controllers/educatorController.js'
+
+const educatorRouter = express.Router()
+
+// Add Educator Role
+educatorRouter.get('/update-role',updateRoleEducator)
+
+export default educatorRouter;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import clerkWebhooks from "./controllers/webhooks.js";
 import connectDB from './configs/mongodb.js'
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+
 // Initialize Express
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,9 +16,11 @@ connectDB();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware())
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
+app.use('/api/educator',express.json(),educatorRouter)
 
 // app.post("/clerk", express.json(), clerkWebhooks);
 app.post(
